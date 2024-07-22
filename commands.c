c27b0ca390811c6ffa40732acdfc8fe928afe1c1
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-07-22 17:31:00 +0200

    added files related to command parsing

---
+++ commands.c
@@ -0,0 +1,35 @@
+#include "commands.h"
+#include "list_command.h"
+#include "view_command.h"
+#include "add_command.h"
+#include "delete_command.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int parse_list_command(int argc, char *argv[]) {
+    return list_vault_logs();
+}
+
+int parse_view_command(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: vaultmgr view <id>\n");
+        return 1;
+    }
+    return view_vault_log(atoi(argv[2]));
+}
+
+int parse_add_command(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: vaultmgr add <text>\n");
+        return 1;
+    }
+    return add_vault_log(argv[2]);
+}
+
+int parse_delete_command(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: vaultmgr delete <id>\n");
+        return 1;
+    }
+    return delete_vault_log(argv[2]);
+}
---
+++ commands.h
@@ -0,0 +1,15 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+typedef struct {
+    const char *name;
+    int (*function)(int argc, char *argv[]);
+    const char *description;
+} Command_t;
+
+int parse_list_command(int argc, char *argv[]);
+int parse_view_command(int argc, char *argv[]);
+int parse_add_command(int argc, char *argv[]);
+int parse_delete_command(int argc, char *argv[]);
+
+#endif //COMMANDS_H
