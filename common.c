aa08ef00f5ef6ea3ce8efbd35a14a10dd47764d4
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-07-22 20:37:38 +0200

    added functions for checking ID and preparing filename

--- common.c
+++ common.c
@@ -1,5 +1,8 @@
 #include "common.h"
 #include <stdio.h>
+#include <limits.h>
+#include <ctype.h>
+#include <string.h>
 
 FILE *check_file(FILE *file, const char *filename) {
     if (file == NULL) {
@@ -9,19 +12,25 @@ FILE *check_file(FILE *file, const char *filename) {
     return file;
 }
 
-int is_valid_id(int id) {
-    if (id <= 0) {
+int is_valid_id(const char *id) {
+    if (id == NULL || strlen(id) == 0 || strlen(id) > MAX_ID_LENGTH) {
         return 0;
     }
 
-    char filename[MAX_ID_LENGTH + 4];
-    snprintf(filename, sizeof(filename), "%d.txt", id);
-
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        return 0;
+    // Provjeri da li su svi karakteri cifre
+    for (const char *p = id; *p; p++) {
+        if (!isdigit(*p)) {
+            return 0;
+        }
     }
-    fclose(file);
 
-    return 1;
+    // Pretvori karakter u u long radi sigurne provjere da li je <= najvecem intu
+    long num_id = atol(id);
+    return (num_id > 0 && num_id <= INT_MAX) ? 1 : 0;
+}
+
+char *prepare_filename(const char *id) {
+    static char filename[FILENAME_MAX_LENGTH];
+    snprintf(filename, sizeof(filename), "%s.txt", id);
+    return filename;
 }
