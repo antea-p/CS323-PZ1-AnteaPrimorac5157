a2a8f53245a4aed237cda96f468f98489468fc8b
Antea P <antea.primorac.5157@metropolitan.ac.rs>
2024-07-22 17:52:13 +0200

    refactored main further so that main function prints usage instructions even for unknown commands

--- main.c
+++ main.c
@@ -3,13 +3,35 @@
 #include <string.h>
 #include "commands.h"
 
-Command_t commands[] = {
+void print_usage(const Command_t *commands_array);
+void print_banner(const char *text);
+
+Command_t commands_array[] = {
     {"list",   parse_list_command,   "Show a summary of all vault logs"},
     {"view",   parse_view_command,   "Read the full contents of the chosen vault log"},
     {"add",    parse_add_command,    "Add a new vault log"},
     {"delete", parse_delete_command, "Delete the chosen vault log"},
     {NULL, NULL, NULL}
 };
+
+int main(int argc, char *argv[]) {
+    print_banner("VAULTMGR v1.0");
+
+    if (argc < 2 || strcmp(argv[1], "help") == 0) {
+        print_usage(commands_array);
+        return 1;
+    }
+
+    for (int i = 0; commands_array[i].name != NULL; i++) {
+        if (strcmp(argv[1], commands_array[i].name) == 0) {
+            return commands_array[i].function(argc, argv);
+        }
+    }
+
+    printf("Unknown command: %s\n", argv[1]);
+    print_usage(commands_array);
+    return 1;
+}
 
 void print_banner(const char *text) {
     // 37 karaktera sirine (eksperimentalna vrijednost, moze se podesiti po potrebi)
@@ -22,24 +44,9 @@ void print_banner(const char *text) {
     printf("\n");
 }
 
-int main(int argc, char *argv[]) {
-    print_banner("VAULTMGR v1.0");
-
-    if (argc < 2) {
-        printf("Usage:\n");
-        for (int i = 0; commands[i].name != NULL; i++) {
-            printf(" - vaultmgr %s - %s\n", commands[i].name, commands[i].description);
-        }
-        return 1;
+void print_usage(const Command_t *commands_array) {
+    printf("Usage:\n");
+    for (int i = 0; commands_array[i].name != NULL; i++) {
+        printf(" - vaultmgr %s - %s\n", commands_array[i].name, commands_array[i].description);
     }
-
-    for (int i = 0; commands[i].name != NULL; i++) {
-        if (strcmp(argv[1], commands[i].name) == 0) {
-            return commands[i].function(argc, argv);
-        }
-    }
-
-    printf("Unknown command: %s\n", argv[1]);
-    return 1;
 }
-
